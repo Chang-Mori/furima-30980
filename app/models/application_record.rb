c27b7f2f6145c7219ae23fb119143d671681226c
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-17 21:29:17 +0900

    バリデーションの編集（パスワードの英数字混合と名前に全角文字とカナ入力を制限）

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
