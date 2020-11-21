26600ca771b5ab6636f11367cdd02e4dbab1ba0d
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-21 18:10:11 +0900

    単体テストコードの追加・コメントアウト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,10 +36,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category is not a number")
       end
 
+      it "カテゴリーのカラムが「---」(id:1)では保存できないこと" do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it "商品状態が空では保存できないこと" do
         @item.status_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Status is not a number")
+      end
+
+      it "商品状態のカラムが「---」(id:1)では保存できないこと" do
+        @item.status_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it "配送料の負担が空では保存できないこと" do
@@ -48,16 +60,34 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Shipping charges is not a number")
       end
 
+      it "配送料の負担のカラムが「---」(id:1)では保存できないこと" do
+        @item.shipping_charges_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charges must be other than 1")
+      end
+
       it "発送元の地域が空では保存できないこと" do
         @item.shipment_source_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipment source is not a number")
       end
 
+      it "発送元の地域のカラムが「---」(id:1)では保存できないこと" do
+        @item.shipment_source_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipment source must be other than 1")
+      end
+
       it "発送までの日数が空では保存できないこと" do
         @item.days_to_ship_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship is not a number")
+      end
+
+      it "発送までの日数のカラムが「---」(id:1)では保存できないこと" do
+        @item.days_to_ship_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
       end
     
       it "販売価格が空では保存できないこと" do
@@ -67,9 +97,15 @@ RSpec.describe Item, type: :model do
       end
 
       it "販売価格が300円未満では保存できないこと" do
-        @item.price = "299"
+        @item.price = '299'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it "販売価格が9999999円超過では保存できないこと" do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
       it "ユーザーが紐づいていないと出品商品が保存できない" do
