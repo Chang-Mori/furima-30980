47d4f7827b27cdccdd692630ffbf8c3e2144fd24
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-25 14:58:08 +0900

    都道府県prefectureをActiveHashを利用するためモデル生成・アソシエーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :prefecture_id, numericality: { other_than: 1 } 
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -3,11 +3,11 @@ class ItemOrder
   attr_accessor :user, :item, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order
 
   with_options presence: true do
-    validates :user, :item, :postal_code, :prefecture, :city, :house_number, :phone_number, :order
+    validates :user, :item, :postal_code, :city, :house_number, :phone_number, :order
   end
 
   def save
     Order.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, :city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
