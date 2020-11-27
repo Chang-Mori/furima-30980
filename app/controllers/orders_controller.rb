223a69ce2f9487df6dfb03c044dfc1f0f3738cc4
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-27 19:58:45 +0900

    インデックスを定義しストロングパラメーターorder_paramsにuser_idとitem_idをマージして値を保存できるよう記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
+    @item = Item.find(params[:item_id])
+    @item_order = ItemOrder.new
   end
 
   def create
     @item_order = ItemOrder.new(order_params)
-    if @item_order.valid?
-      @item_order.save
-      redirect_to action: index
-    end
+    @item_order.save
+    redirect_to root_path
   end
 
   private
   def order_params
-    params.require(:item_order).permit(:user, :item, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order)
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
 end
