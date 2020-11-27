3aef1ab3f9041519c95641e2f1f772c7e7040ce9
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-27 19:56:45 +0900

    formオブジェクトItemOrderクラスの修正とバリデーションの集約

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,4 @@ class Address < ApplicationRecord
   belongs_to :order
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-
-  validates :prefecture_id, numericality: { other_than: 1 } 
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,13 +1,18 @@
 class ItemOrder
-  include ActiveModel::ActiveModel
-  attr_accessor :user, :item, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
-    validates :user, :item, :postal_code, :city, :house_number, :phone_number, :order
+    validates :postal_code, :city, :house_number, :phone_number, :image, :name, :explanation
   end
 
+  validates :price, numericality:{ only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :category_id, :status_id, :shipping_charge_id, :shipment_source_id, :days_to_ship_id, :prefecture_id, numericality: { other_than: 1 } 
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+
   def save
-    Order.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    #order.id => 1
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
