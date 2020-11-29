fea8db583edba2442c4bc1f4d127927c97298713
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-29 18:55:39 +0900

    購入後sold outが表示され、直接アドレスを入力しても遷移できないように追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    if user_signed_in? && current_user.id != @item.user_id
+    if user_signed_in? && current_user.id != @item.user_id && @item.order.nil?
       @item_order = ItemOrder.new
     else
       redirect_to root_path
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
+      pay_item
       @item_order.save
       redirect_to root_path
     else
@@ -23,6 +24,15 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, price: params[:price], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
