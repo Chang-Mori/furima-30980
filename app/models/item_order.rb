c22ea7aaf283f3589b6a507e14e4a22622ee9197
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-29 18:54:29 +0900

    クレジットカード購入機能実装

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,14 +1,14 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token, :price
 
   with_options presence: true do
-    validates :postal_code, :city, :house_number, :phone_number
+    validates :postal_code, :city, :house_number, :phone_number, :token
   end
 
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: { other_than: 1 }
-  validates :phone_number, format: {with: /\A\d{11}\z/}
+  validates :phone_number, format: {with: /\A\d{10,11}\z/}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
