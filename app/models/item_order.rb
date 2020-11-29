744d9a920d1234c9e7882897a0d56b8eff5bc182
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-11-29 21:15:24 +0900

    テストコードにcontextを記述し、コメントアウトを削除

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -12,7 +12,6 @@ class ItemOrder
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    #order.id => 1
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
