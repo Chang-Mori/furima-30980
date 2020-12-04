babab18bb3ce3eedf555d8bdb2592ce4959348da
Chang-Mori <n0musi9.n0rice@gmail.com>
2020-12-04 23:15:08 +0900

    記述違いのためgithubと連携できずエラー。修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "furima-30980"
-set :repo_url, "git@example.com:Chang-Mori/furima-30980.git"
+set :repo_url, "git@github.com:Chang-Mori/furima-30980.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
